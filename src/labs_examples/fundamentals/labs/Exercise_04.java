a69de180bc7dfdd7713f8b8b74439a67456c6b57
Riley Thurm <rileyt@laridian.com>
2020-07-14 19:06:19 -0500

    fundamentals exercises finished (mostly)

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,25 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
+        // write your code below
+
         int i = 1;
         System.out.println("int i is: " + i);
+        boolean b = true;
+        System.out.println("boolean b is: " + b);
+        char a = 'a';
+        System.out.println("char a is: " + a);
+        short y = 273;
+        System.out.println("short y is: " + y);
+        byte z = 14;
+        System.out.println("byte z is: " + z);
+        long l = 25468945l;
+        System.out.println("long l is: " + l);
+        double d = 45874958.54;
+        System.out.println("double d is: " + d);
+        float f = 45.8f;
+        System.out.println("float f is: " + f);
 
-        // write your code below
 
     }
 
